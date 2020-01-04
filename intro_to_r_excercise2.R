2febfc593c782568a6507adf82ed70cd4d681135
Kristin Eccles <kristin.eccles@gmail.com>
2020-01-03 22:16:21 -0500

    Add files via upload

---
+++ intro_to_r_excercise2.R
@@ -0,0 +1,39 @@
+#############################################################
+# Exercise 2 - Introduction to R 
+#############################################################
+# Load Libraries
+library(datasets)
+
+# Load data
+# the dataset we will use is a a dataset built into R and be called from the 
+# datasets package
+data(airquality)
+
+#############################################################
+# Summarize the dataframe
+
+# Check for missing data and address it if necessary
+
+#############################################################
+# Visualize the data 
+# Make a histogram of the variable ozone
+
+
+# Data distribution ozone by subset month
+
+
+# Boxplot of ozone by month, with whiskers
+# Hint: What type of variable is month? Boxplots require 1 cateogrical variables and 
+# 1 continuous variable
+
+
+# Scatterplot of ozone as a function of temperature
+
+
+# Now colour the points by month
+
+
+# Matrix of scatterplots of all pairs of variables in air quality dataset
+
+
+# Think of some hyoptheses that that could be tested. We will work on these next week. 
