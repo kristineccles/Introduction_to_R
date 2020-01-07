7e4a3a21f839fc0c2ec9e05ab5c7791df1d4973b
Kristin Eccles <kristin.eccles@gmail.com>
2020-01-07 10:32:16 -0500

    address missing data

--- intro_to_R_exercise1.R
+++ intro_to_R_exercise1.R
@@ -14,26 +14,25 @@ library(ggplot2)
 sturgeon= read.csv("sturgeon.csv")
 
 #############################################################
-# Check to see if we hav emissing data
+# Check to see if we have emissing data
 is.na(sturgeon)
-
-# Clean the data if necessary
-# if we had missing data (which we dont in this dataframe) we could remove 
-# create new dataset without missing data 
-# newdata <- na.omit(mydata)
 
 # Summarize the contents of the sturgeon dataframe
 summary(sturgeon)
 # you will also see missing data in this summary 
 
+# Clean the data (if necessary)
+# create new dataset without missing data 
+sturgeon_clean=na.omit(sturgeon)
+
 #############################################################
 # Visualize the data 
 # Make a histogram of the variable fklngth in the sturgeon
 # this uses the base R plotting 
-hist(sturgeon$fklngth)
+hist(sturgeon_clean$fklngth)
 
 # Make variables in the sturgeon data.frame directly callable by attaching
-attach(sturgeon)
+attach(sturgeon_clean)
 
 # Redo the same histogram as before
 ls()
@@ -42,7 +41,7 @@ hist(fklngth)
 # use "sturgeon" dataframe to make plot called mygraph
 # and define x axis as representing fklngth
 
-plot1 <- ggplot(sturgeon) +
+plot1 <- ggplot(sturgeon_clean) +
   geom_histogram(aes(x = fklngth, y = ..density..),
                  binwidth = 10, fill = "grey", color = "black")
 plot1
@@ -50,7 +49,7 @@ plot1
 
 # Data distribution fklngth by subsets of sex and year
 # split previous graph per year (rows) and sex (columns)
-plot2 <- ggplot(sturgeon) +
+plot2 <- ggplot(sturgeon_clean) +
   geom_histogram(aes(x = fklngth, y = ..density..),
                  binwidth = 5, fill = "grey", color = "black")+
   facet_grid(sex ~ year)
@@ -69,7 +68,7 @@ shapiro.test(fklngth)
 boxplot(fklngth ~ sex, notch = TRUE)
 
 #ggplot2 version
-plot3<-ggplot(sturgeon, aes(x=sex, y=fklngth))
+plot3<-ggplot(sturgeon_clean, aes(x=sex, y=fklngth))
 plot3+ geom_boxplot()
 
 # Scatterplot of fklngth as a function of age
@@ -77,17 +76,17 @@ plot3+ geom_boxplot()
 plot(fklngth ~ age)
 
 #ggplot2 version
-plot4<-ggplot(sturgeon, aes(x=age, y=fklngth))
+plot4<-ggplot(sturgeon_clean, aes(x=age, y=fklngth))
 plot4+ geom_point()
 
 # Matrix of scatterplots of all pairs of variables in dataframe sturgeon, with lowess trace
 pairs(sturgeon, panel = panel.smooth)
 
 # Create a subset with only females captured in 1978
-sturgeon.female.1978 <- subset(sturgeon, sex == "FEMALE" &
+sturgeon.female.1978 <- subset(sturgeon_clean, sex == "FEMALE" &
                                  year == "1978")
 
 # Create a histogram of fklngth from a subset for females in 1979 and 1980
-mysubset = subset(sturgeon, sex == "FEMALE" & (year == "1979" | year == "1980"))
+mysubset = subset(sturgeon_clean, sex == "FEMALE" & (year == "1979" | year == "1980"))
 hist(mysubset$fklngth)
 
